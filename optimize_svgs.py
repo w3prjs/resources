f9ebd78c329b17cdb3368d48315afb3090ed47a9
w3prjs <>
2024-03-16 12:55:21 +0200

    Update

--- optimize_svgs.py
+++ optimize_svgs.py
@@ -7,5 +7,6 @@ for root, subdirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
 
         p_in  = os.path.join(root, file)
         p_out = p_in.replace('.svg', '-min.svg')
+        os.remove(p_out)
 
         subprocess.getoutput(f'svgo {p_in} -o {p_out}')
